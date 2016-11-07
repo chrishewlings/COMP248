ec01dee77338bfb4a4a54ff6da71c2bf1197fb92
Chris Hewlings <chris.hewlings@gmail.com>
2016-11-07 11:05:46 -0500

    Finished question 3

--- A3/A3Q3.java
+++ A3/A3Q3.java
@@ -1,70 +1,59 @@
-class Scale
-{
-	double weight;
-	String unit;
-
-	double getWeightKg()
-	{
-		if(this.unit == "lbs")
-			return (this.weight / 2.2);
-		else return this.weight;
-	}
-
-	double getWeightLb()
-	{
-		if(this.unit == "lbs")
-			return this.weight;
-		else return (this.weight * 2.2);
-	}
-
-	void setWeight(double weight)
-	{
-		this.weight = weight;
-	}
-
-	void setWeightAndUnit(double weight, String unit)
-	{
-		this.weight = weight;
-		this.unit = unit;
-	}
-
-	public Scale()
-	{
-		weight = 0.0;
-		unit = "kg";
-	}
-
-	public Scale(double weight)
-	{
-		weight = this.weight;
-		unit = "kg";
-	}
-	public Scale(double weight, String unit)
-	{	
-		weight = this.weight;
-		unit = this.unit;
-	}
-	public int waterIntake()
-	{
-		if(this.unit == "kg")
-			double weightInLbs = (this.weight * 2.2);
-		else weightInLbs = this.weight;
-
-		return ( (int) weightInLbs / 2)
-		
-
-		return ((int) weight / 2)
-	}
-
-
-
-
-}
-
-public class A3Q3
+public class A3Q3 
 {
 	public static void main(String[] args)
 	{
+		// set up 3 objects of type Scale
+
+		Scale jim = new Scale(176,"lbs");
+		Scale michael = new Scale(86,"kg");
+		Scale stanley = new Scale(105,"kg");
+
+		
+		// print their weights in KG
+
+		System.out.printf("\nWEIGHTS IN KILOGRAMS:\n");
+		System.out.printf("---------------------\n");
+
+		System.out.printf("Jim weighs %.2fkg\n", jim.getWeightInKg());
+		System.out.printf("Michael weighs %.2fkg\n", michael.getWeightInKg());
+		System.out.printf("Stanley weighs %.2fkg\n", stanley.getWeightInKg());
+
+		// print their weights in LB
+		
+		System.out.printf("\nWEIGHTS IN POUNDS:\n");
+		System.out.printf("------------------\n");
+
+		System.out.printf("Jim weighs %.2flbs\n", jim.getWeightInLbs());
+		System.out.printf("Michael weighs %.2flbs\n", michael.getWeightInLbs());
+		System.out.printf("Stanley weighs %.2flbs\n", stanley.getWeightInLbs());
+		
+		// Based on their daily exercise routines, display recommended water intake
+
+		System.out.printf("\nRECOMMENDED WATER INTAKE:\n");
+		System.out.printf("---------------------------\n");
+
+		System.out.printf("Jim exercises 90 minutes a day, and should drink %.2f cups of water.\n", jim.waterIntakeWithExercise(90));
+		System.out.printf("Michael exercises 30 minutes a day, and should drink %.2f cups of water.\n", michael.waterIntakeWithExercise(30));
+		System.out.printf("Stanley doesn't exercise, and should drink %.2f cups of water.\n", stanley.waterIntake());
+
+
+		System.out.printf("\n\n");
+
+		// Using mutator method to increase weight of one of the Scale objects
+
+		jim.setWeightAndUnit(jim.getWeightInKg() + 6.0, "kg");
+		System.out.printf("Jim gained 6 kilograms.\n");
+
+		// Using boolean methods to compare two instances of Scale object
+
+		if(jim.equals(michael))
+			System.out.printf("Jim now weighs the same as Michael.\n");
+		if(michael.isLessThan(stanley))
+			System.out.printf("Michael weighs less than Stanley.\n");
+		if(stanley.isGreaterThan(jim))
+			System.out.printf("Stanley weighs more than Jim.\n");
+
+
 
 	}
 }
---
+++ A3/Scale.java
@@ -0,0 +1,115 @@
+public class Scale
+{
+	// instance variables
+
+	private double weight;
+	private String unit;
+	final int OZ_PER_CUP = 8;
+	final double KG_PER_LB = 1/2.2;
+	final double LB_PER_KG = 2.2;
+
+	//constructor methods
+
+
+	public Scale()
+	{
+		weight = 0;
+		unit = "kg";
+	}
+
+	public Scale(double weight)
+	{
+		this.weight = weight;
+		unit = "kg";
+	}
+
+	public Scale(double weight, String unit)
+	{
+		this.weight = weight;
+		this.unit = unit;
+	}
+
+	//accessor methods
+
+	public double getWeightInKg()
+	{
+		if(unit == "kg")
+			return weight;
+		else return (weight * KG_PER_LB);
+	}
+
+	public double getWeightInLbs()
+	{
+		if(unit == "lbs")
+			return weight;
+		else return (weight * LB_PER_KG);
+	}
+
+	// mutator methods
+
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
+
+	public void setWeightAndUnit(double weight, String unit)
+	{
+		this.weight = weight;
+		this.unit = unit;
+	}
+
+	// other methods
+
+	public double waterIntake()
+	{
+		double waterIntake;
+		double weightInLbs;
+
+		if(unit == "lbs")
+		{
+			waterIntake = weight / 2 / OZ_PER_CUP;
+			return waterIntake;
+		}
+		else
+			weightInLbs = weight * LB_PER_KG;
+			waterIntake = ((weightInLbs / 2) / OZ_PER_CUP);
+			return waterIntake;
+	}
+
+	public double waterIntakeWithExercise(double minutesOfExercise)
+	{
+		double baseIntake = waterIntake();
+		double additionalIntake = (12 * (minutesOfExercise / 30)) / OZ_PER_CUP ;
+		
+		return (baseIntake + additionalIntake);
+
+	}
+
+	// override methods -- overrides methods that would be inherited from other classes
+
+	public String toString() // prints a stringified version of the instances variables
+	{
+		return (weight+unit);
+	}
+
+	public boolean equals(Scale secondObj) // compares two objects and returns true if they're equal
+	{
+		if(secondObj.getWeightInLbs() == this.getWeightInLbs())
+			return true;
+		else return false;
+	}
+
+	public boolean isLessThan(Scale secondObj) // compares two objects and returns true obj1 < obj2
+	{
+		if(this.getWeightInLbs() < secondObj.getWeightInLbs())
+			return true;
+		else return false;
+	}
+
+	public boolean isGreaterThan(Scale secondObj) // compares two objects and returns true obj 1 > obj2
+	{
+		if(this.getWeightInLbs() > secondObj.getWeightInLbs())
+			return true;
+		else return false;
+	}
+}
